6d2441d91aa6d00ee91d90197d7a25f92bcb80b6
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-09-12 17:43:54 +0200

    remove crx signature attempt with yauzl

--- extensionsManager.ts
+++ extensionsManager.ts
@@ -1,7 +1,8 @@
 // @ts-ignore
 import { remote } from 'electron';
 import fs from "fs";
-import AdmZip from 'adm-zip';
+import * as yauzl from "yauzl";
+import * as path from "path";
 
 export class ExtensionsManager {
 	constructor() {}
@@ -17,16 +18,61 @@ export class ExtensionsManager {
 
   async loadCrxExtension(crxPath: string) {
     try {
-      const zip = new AdmZip(crxPath);
       const tempDir = './tempExtension';
-      zip.extractAllTo(tempDir, true);
 
-      await this.loadExtension(tempDir);
+      const zipData = await this.removeCrxSignature(crxPath);
 
-      // Supprimer le dossier temporaire après avoir chargé l'extension
-      fs.rmdirSync(tempDir, { recursive: true });
+      yauzl.fromBuffer(zipData, {lazyEntries: true}, (err, zipfile) => {
+        if (err) throw err;
+        zipfile.readEntry();
+        zipfile.on("entry", (entry) => {
+          const filePath = path.join(tempDir, entry.fileName);
+          zipfile.openReadStream(entry, (err, readStream) => {
+            if (err) throw err;
+            fs.mkdirSync(path.dirname(filePath), {recursive: true});
+            readStream.pipe(fs.createWriteStream(filePath));
+            readStream.on("end", () => {
+              zipfile.readEntry();
+            });
+          });
+        });
+        zipfile.on("end", async () => {
+          await this.loadExtension(tempDir);
+          // Supprimer le dossier temporaire après avoir chargé l'extension
+          fs.rmdirSync(tempDir, { recursive: true });
+        });
+      });
     } catch (error) {
       console.error('Failed to load .crx extension:', error);
     }
   }
+
+
+  removeCrxSignature(crxPath: string): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      fs.readFile(crxPath, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const zipData = data.slice(16); // Supprime les 16 premiers octets (signature .crx)
+          resolve(zipData);
+        }
+      });
+    });
+  }
+
+  // async loadCrxExtension(crxPath: string) {
+  //   try {
+  //     const zip = new AdmZip(crxPath);
+  //     const tempDir = './tempExtension';
+  //     zip.extractAllTo(tempDir, true);
+  //
+  //     await this.loadExtension(tempDir);
+  //
+  //     // Supprimer le dossier temporaire après avoir chargé l'extension
+  //     fs.rmdirSync(tempDir, { recursive: true });
+  //   } catch (error) {
+  //     console.error('Failed to load .crx extension:', error);
+  //   }
+  // }
 }
